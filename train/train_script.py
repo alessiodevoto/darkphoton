11dd98c0c5153c75818a4c1646d4e3a17cdd39c3
Donatella Genovese <donatellagen@gmail.com>
2025-05-11 14:06:18 +0200

    Update train_script.py

--- train/train_script.py
+++ train/train_script.py
@@ -9,10 +9,11 @@ import os
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from torchmetrics.classification import AUROC  # don't forget to import this if using torchmetrics
 
-from train import train_evaluate, evaluate
+from train.train import train_evaluate, evaluate
 from utils import EmbedEncode, LoadBalancingLoss
-from dataset import CustomEventsDataset2, MakeHomogeneous
-from model import Transformer
+from dataset.darkphotondataset import DarkPhotonDataset
+from dataset.transforms import GraphFilter
+from models.model import Transformer
 from sklearn.model_selection import train_test_split
 
 # ---- SETUP for evaluation ----
@@ -28,24 +29,23 @@ print(f"Using device: {device}")
 
 # ---- CONFIGURATION ----
 encoding_size = 4
-trials = 10
 
-input_size = 12
-hidden_size = 80
-g_norm = True
-heads = 2
+input_size = 4
+hidden_size = 60
+g_norm = False
+heads = 4
 num_xprtz = 6
-xprt_size = 20
+xprt_size = 30
 k = 2
 dropout_encoder = 0.0
-layers = 2
+layers = 4
 output_size = 2
 w_load = 1
 batchsize = 500
 epochs = 60
 patience = 45
 gamma = 0.1
-learning_rate = 0.001
+learning_rate = 0.005
 stepper = True
 con = str(str(input_size)+str(hidden_size)+str(encoding_size)+str(g_norm)+str(heads)+str(num_xprtz)+str(xprt_size)+str(k)+str(dropout_encoder)+str(layers)+str(output_size)+str(w_load)+str(batchsize)+str(epochs)+str(patience)+str(gamma)+str(learning_rate)+str(stepper)+str("prova"))
 
@@ -67,16 +67,14 @@ dataset_root = './data'
 os.makedirs(dataset_root, exist_ok=True)
 
 # ---- LOAD DATA ----
-datasetfull = CustomEventsDataset2(
-    root=dataset_root,
-    url='https://cernbox.cern.ch/s/0nh0g7VubM4ndoh/download',
-    k=encoding_size,
-    delete_raw_archive=False,
-    add_edge_index=True,
-    event_subsets={'signal': 400, 'singletop': 200, 'ttbar': 200},
-    transform=MakeHomogeneous(), 
-    signal_filter=lambda filename: "Wh_hbb_fullMix.h5" in filename
-)
+graph_filter = GraphFilter(min_num_nodes=2) # only graphs with at least 2 nodes will be accepted
+dataset = DarkPhotonDataset(root=dataset_root, 
+                            subset=0.5,
+                            url="https://cernbox.cern.ch/s/PYurUUzcNdXEGpz/download",
+                            pre_filter = graph_filter,
+                            pre_transform = None,
+                            post_filter = None, 
+                            verbose=True)
 
 trainset, testset = train_test_split(datasetfull, test_size=0.2)
 trainset, evalset = train_test_split(trainset, test_size=0.2)
